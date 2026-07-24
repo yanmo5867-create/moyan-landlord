1509d87bfe17bf009dca8e5990d5c67f0e2a047f
yanmo5867-create <yanmo5867@gmail.com>
2026-07-24 20:57:17 +0800

    fix: rebuild complete project structure to fix compilation

--- src/com/moyan/landlord/ui/MainActivity.java
+++ src/com/moyan/landlord/ui/MainActivity.java
@@ -1,9 +1,25 @@
+
 package com.moyan.landlord.ui;
+
 import android.app.Activity;
+
 import android.os.Bundle;
+
 public class MainActivity extends Activity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        System.out.println("Moyan Landlord App Started!");
-    }
+
+@Override
+
+protected void onCreate(Bundle savedInstanceState) {
+
+super.onCreate(savedInstanceState);
+
+// 确保 res/layout/activity_main.xml 存在
+
+setContentView(R.layout.activity_main);
+
+System.out.println("Moyan Landlord App Started!");
+
 }
+
+}
+
